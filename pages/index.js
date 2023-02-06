43c3546fd8066bfcc861264e6cec11544752e560
nazmul-2020 <nazmulcpi2012@gmail.com>
2023-02-06 13:11:25 +0600

    addea navbar

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,19 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Banner from '@/Components/Banner/Banner'
+import Navbar from '@/Components/Banner/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
-      <h1>hi</h1>
-
-      <h1 className="text-3xl text-red-600 font-bold underline">
-        Hello world!
-      </h1>
+    <div className={styles.bg_img}>
+    <div className='mx-[99px]'>
+     
+      <Navbar/>
       <Banner />
 
+    </div>
     </div>
   )
 }
